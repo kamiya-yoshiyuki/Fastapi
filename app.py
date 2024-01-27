5948c1978ae342b199990d618c2107d63939a5ed
kamiya <kamiya.yoshiyuki0604@gmail.com>
2024-01-27 09:55:00 +0900

    1st commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+
+st.title('インフルエンザ 分類')
+smiles = st.text_input('smiles を入力')
+response = requests.post(url='http://127.0.0.1:8000/make_predictions', json={'smiles': smiles})
+# print(response.status_code)
+st.write(smiles)
+
+target = ['効果あり', '不明', '効果なし']
+
+prediction = response.json()['prediction']
+
+st.write('## prediction')
+st.write('#### この化合物はインフルエンザに対して「', str(target[int(prediction)]),'」です')
+
+# st.write('0 : High（効果あり）')
+# st.write('1 : medium（どちらともいえない）')
+# st.write('2 : low（効果なし）')
