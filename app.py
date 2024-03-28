4f552336c9c147b223f5c6a27dbd8be164fe600c
kamiya-yoshiyuki <157268518+kamiya-yoshiyuki@users.noreply.github.com>
2024-03-28 14:41:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 
 st.title('インフルエンザ 分類')
-st.title('test')
 smiles = st.text_input('smiles を入力')
 if st.button('判定'):
     response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
     # print(response.status_code)
     mol = Chem.MolFromSmiles(smiles)
-    st.write(mol)
-
+    img = mol_to_image(mol)
+    st.image(img, use_column_width=True)
+    
     target = ['効果あり', '不明', '効果なし']
 
     prediction = response.json()['prediction']
