fff5538b68db06f7b6f75046d93ddfea05666e18
kamiya-yoshiyuki <157268518+kamiya-yoshiyuki@users.noreply.github.com>
2024-03-28 14:52:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if st.button('判定'):
     if response.status_code == 200:
         mol = Chem.MolFromSmiles(smiles)
         
-        if mol is not None:  # mol オブジェクトが None でない場合にのみ処理を続行する
+        if mol is not None:
             img = mol_to_image(mol)
             st.image(img, use_column_width=True)
             
@@ -24,7 +24,7 @@ if st.button('判定'):
             st.write('## 結果')
             st.write(f'この化合物はインフルエンザに対して「{target[int(prediction)]}」です')
         else:
-            st.error('SMILES文字列が不正で、化学構造を生成できませんでした。')
+            st.error('指定された SMILES 文字列は不正です。')
     else:
         st.error('予測の取得中にエラーが発生しました。')
 
