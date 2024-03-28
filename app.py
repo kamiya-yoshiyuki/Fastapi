9ab3d8b4e3dfbe6e78910c9eae21eac4ff11c039
kamiya-yoshiyuki <157268518+kamiya-yoshiyuki@users.noreply.github.com>
2024-03-28 15:49:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,47 @@ import requests
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import Draw
-from rdkit.Chem.Draw import rdMolDraw2D
 from rdkit.Chem import AllChem
 from PIL import Image
 
-st.title('インフルエンザ 分類')
-smiles = st.text_input('smiles を入力')
 
-if st.button('判定'):
-    response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
+st.title('SMILESからの分子画像表示')
+
+# SMILESを入力するテキストボックスを作成
+smiles = st.text_input('SMILESを入力してください')
+
+#if st.button('画像を表示'):
+    # 入力されたSMILESから分子を作成
+ #   mol = Chem.MolFromSmiles(smiles)
     
-    if response.status_code == 200:
-        mol = Chem.MolFromSmiles(smiles)
+  #  if mol is not None:
+        # 分子の描画
+   #     img = Draw.MolToImage(mol)
         
-        if mol is not None:
-            img = Chem.Draw.MolToImage(mol)
-            st.image(img, caption='SMILESから生成された分子画像', use_column_width=True)
+        # 画像をStreamlitアプリに表示
+    #    st.image(img, caption='SMILESから生成された分子画像', use_column_width=True)
+    #else:
+    #    st.error('指定されたSMILESは無効です。')
+#st.title('インフルエンザ 分類')
+#smiles = st.text_input('smiles を入力')
+
+#if st.button('判定'):
+ #   response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
+    
+ #   if response.status_code == 200:
+  #      mol = Chem.MolFromSmiles(smiles)
+        
+   #     if mol is not None:
+  #          img = Chem.Draw.MolToImage(mol)
+  #          st.image(img, caption='SMILESから生成された分子画像', use_column_width=True)
             
-            target = ['効果あり', '不明', '効果なし']
-            prediction = response.json()['prediction']
+    #        target = ['効果あり', '不明', '効果なし']
+      #      prediction = response.json()['prediction']
             
-            st.write('## 結果')
-            st.write(f'この化合物はインフルエンザに対して「{target[int(prediction)]}」です')
-        else:
-            st.error('指定された SMILES 文字列は不正です。')
-    else:
-        st.error('予測の取得中にエラーが発生しました。')
+     #       st.write('## 結果')
+          #  st.write(f'この化合物はインフルエンザに対して「{target[int(prediction)]}」です')
+        #else:
+        #    st.error('指定された SMILES 文字列は不正です。')
+    #else:
+       # st.error('予測の取得中にエラーが発生しました。')
 
