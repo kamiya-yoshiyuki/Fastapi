4d39234f2df5782a93343bb0cc7459f6920eebc0
kamiya-yoshiyuki <157268518+kamiya-yoshiyuki@users.noreply.github.com>
2024-03-28 14:14:32 +0900

    Update app.py
    streamlitのレイアウトを変更
    化合物の画像が出るようにした
    2024/3/28

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import streamlit as st
 import requests
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import IPythonConsole
 
 st.title('インフルエンザ 分類')
 smiles = st.text_input('smiles を入力')
-response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
-# print(response.status_code)
-st.write(smiles)
+if st.button('判定'):
+    response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
+    # print(response.status_code)
+    mol = Chem.MolFromSmiles(smiles)
+    st.write(mol)
 
-target = ['効果あり', '不明', '効果なし']
+    target = ['効果あり', '不明', '効果なし']
 
-prediction = response.json()['prediction']
+    prediction = response.json()['prediction']
 
-st.write('## prediction')
-st.write('#### この化合物はインフルエンザに対して「', str(target[int(prediction)]),'」です')
+    st.write('## 結果')
+    st.write('#### この化合物はインフルエンザに対して「', str(target[int(prediction)]),'」です')
 
-# st.write('0 : High（効果あり）')
-# st.write('1 : medium（どちらともいえない）')
-# st.write('2 : low（効果なし）')
+    # st.write('0 : High（効果あり）')
+    # st.write('1 : medium（どちらともいえない）')
+    # st.write('2 : low（効果なし）')
