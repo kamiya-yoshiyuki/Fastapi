999068eb5b16e09f66d824df1e4f3c52cd00448a
kamiya-yoshiyuki <157268518+kamiya-yoshiyuki@users.noreply.github.com>
2024-03-28 17:40:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
 import streamlit as st
 import requests
 import rdkit
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit.Chem import Draw
-from PIL import Image
 
-import pickle
-import datamaker
-from rdkit.Chem.Draw import SimilarityMaps
-from io import BytesIO
-from functools import partial
-from rdkit.Chem.Draw import rdDepictor
 
 st.title('インフルエンザ 分類')
 smiles = st.text_input('smiles を入力')
 
 if st.button('判定'):
     response = requests.post(url='https://infuluenza-classifier.onrender.com/make_predictions', json={'smiles': smiles})
+    st.write(smiles)
     
-    if response.status_code == 200:
-        mol = Chem.MolFromSmiles(smiles)
-        
-        if mol is not None:
-            img = Chem.Draw.MolToImage(mol)
-            st.image(img, caption='SMILESから生成された分子画像', use_column_width=True)
-            
-            target = ['効果あり', '不明', '効果なし']
-            prediction = response.json()['prediction']
-            
-            st.write('## 結果')
-            st.write(f'この化合物はインフルエンザに対して「{target[int(prediction)]}」です')
-        else:
-            st.error('指定された SMILES 文字列は不正です。')
-    else:
-        st.error('予測の取得中にエラーが発生しました。')
-
+    target = ['効果あり', '不明', '効果なし']
+    
+    prediction = response.json()['prediction']
+    
+    st.write('## 予測結果')
+    
+    st.write('#### この化合物はインフルエンザに対して「', str(target[int(prediction)]),'」です')
+    
+    # st.write('0 : High（効果あり）')
+    # st.write('1 : medium（どちらともいえない）')
+    # st.write('2 : low（効果なし）')
